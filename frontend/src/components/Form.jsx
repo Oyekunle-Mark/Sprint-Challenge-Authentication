6899606d49d7d9f9be7c8c9167a9e9af8fb61d79
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 16:53:16 +0100

    Styled the reusable form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,37 @@
 import React, { createRef } from 'react';
 import { func, string } from 'prop-types';
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  max-width: 270px;
+  background: #ecbe13;
+  padding: 30px;
+  margin: 20px;
+  border-radius: 5px;
+
+  input {
+    width: 250px;
+    height: 25px;
+    margin: 10px 0;
+    border: 1px solid #634c03;
+    border-radius: 3px;
+    padding: 3px;
+  }
+
+  button {
+    border: 1px solid #ffffff;
+    background: #ffffff;
+    height: 25px;
+    width: 80px;
+    margin: 10px 0;
+    font-size: 12px;
+    border-radius: 3px;
+  }
+`;
 
 const Form = ({ handleSubmit, type }) => {
   const username = createRef();
@@ -15,11 +47,11 @@ const Form = ({ handleSubmit, type }) => {
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <StyledForm onSubmit={submitForm}>
       <input type="text" placeholder="username" ref={username} />
       <input type="password" placeholder="password" ref={password} />
       <button type="submit">{type}</button>
-    </form>
+    </StyledForm>
   );
 };
 
