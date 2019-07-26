bd157329e442efcec02e29f14b3438f3a18f9f60
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 08:23:56 +0100

    Added a logger, welcome and error message

--- api/server.js
+++ api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('morgan');
 
 const configureRoutes = require('../config/routes.js');
 
 const server = express();
 
-server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(logger('dev'));
 
-configureRoutes(server);
+server.get('/', (req, res) =>
+  res.status(200).json({
+    status: 200,
+    message: 'Welcome, O faithful developer.',
+  }),
+);
+
+server.use((req, res) =>
+  res.status(404).json({
+    status: 404,
+    message: 'That URL will take you nowhere',
+  }),
+);
 
 module.exports = server;
