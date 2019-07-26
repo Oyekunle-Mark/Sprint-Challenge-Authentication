eb9ae4c9195d9133dea8cf829bb51abab9589ab8
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 16:54:43 +0100

    Styled the jokes component

--- frontend/src/components/Jokes.jsx
+++ frontend/src/components/Jokes.jsx
@@ -1,5 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const StyledJokes = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h4 {
+    font-family: 'Rajdhani', sans-serif;
+    margin: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  ul li {
+    background: #ecbe13;
+    width: 70vw;
+    margin: 10px 0;
+    padding: 15px;
+    font-size: 14px;
+    font-family: sans-serif;
+    border-radius: 3px;
+  }
+`;
 
 const Jokes = () => {
   const [jokes, setJokes] = useState([]);
@@ -23,11 +48,11 @@ const Jokes = () => {
   const jokeList = jokes.map(jk => <li key={jk.id}>{jk.joke}</li>);
 
   return (
-    <div>
-      <h1>Joke Collection</h1>
+    <StyledJokes>
+      <h4>Joke Collection</h4>
       <h2>{!jokes.length && current}</h2>
       <ul>{jokeList}</ul>
-    </div>
+    </StyledJokes>
   );
 };
 
