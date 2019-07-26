88fd3d308050dc08f7ba14643b49148beab8daa0
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 10:09:05 +0100

    Middleware for the auth route

--- auth/route.js
+++ auth/route.js
@@ -1,7 +1,8 @@
 const route = require('express').Router();
 const handlers = require('./handlers');
+const middleware = require('./authMiddleware');
 
-route.post('/register', handlers.register);
-route.post('/login', handlers.login);
+route.post('/register', middleware, handlers.register);
+route.post('/login', middleware, handlers.login);
 
 module.exports = route;
