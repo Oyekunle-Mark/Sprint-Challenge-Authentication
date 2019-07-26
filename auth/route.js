7cc9decc4a06cf5c4273e3666440d6cb687270c3
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 09:59:40 +0100

    Login endpoint

--- auth/route.js
+++ auth/route.js
@@ -2,5 +2,6 @@ const route = require('express').Router();
 const handlers = require('./handlers');
 
 route.post('/register', handlers.register);
+route.post('/login', handlers.login);
 
 module.exports = route;
