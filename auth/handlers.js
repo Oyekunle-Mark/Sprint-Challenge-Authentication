d9f9783b847e14f2cff04d35d4d6f7be4b9f1842
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 09:59:20 +0100

    The login route handler

--- auth/handlers.js
+++ auth/handlers.js
@@ -1,5 +1,6 @@
 const Model = require('./authModel');
 const bcryptHelpers = require('../helpers/bcryptHelpers');
+const jwtHelpers = require('../helpers/jwtHelpers');
 
 const register = async (req, res) => {
   const { username } = req.body;
@@ -21,6 +22,42 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await Model.getByUsername(username);
+
+    if (!user)
+      return res.status(401).json({
+        status: 401,
+        message: 'Incorrect username.',
+      });
+
+    const match = await bcryptHelpers.compare(password, user.password);
+
+    if (!match)
+      return res.status(401).json({
+        status: 401,
+        message: 'Incorrect password.',
+      });
+
+    const token = jwtHelpers.sign({ id: user.id });
+
+    res.status(200).json({
+      status: 200,
+      token,
+      message: 'Welcome, step right in.',
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      error: 'Error login you in.',
+    });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
