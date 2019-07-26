9208e30102c40924c548b6f9edab39019919dab4
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-26 16:52:49 +0100

    Styled the container app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import LandingPage from './components/LandingPage';
 import Jokes from './components/Jokes';
 import WithAuth from './components/WithAuth';
+import Header from './components/Header';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background: #fde6bd;
+  min-height: 100vh;
+`;
 
 const App = () => (
   <Router>
-    <div>
-      <h1>Jokes</h1>
+    <StyledApp>
+      <Header />
       <Route exact path="/" component={LandingPage} />
       <WithAuth path="/jokes" component={Jokes} />
-    </div>
+    </StyledApp>
   </Router>
 );
 
